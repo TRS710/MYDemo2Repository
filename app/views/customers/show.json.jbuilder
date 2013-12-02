97e23dd18a7afab61edd5382d90a59fb2ce26c97
FujitsuPOCUser <rakesh.shinde@in.fujitsu.com>
2013-12-02 10:55:15 +0530

    Initailise repo

---
+++ app/views/customers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @customer, :name, :Description, :Product, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo2::Application.config.secret_key_base = 'd5f1263efd1d8fedb25b00194e45fe2cbb9945e95b95cb0674ea11423328abbb200bb760b7b3906733d4d6736ab29cca4fb8cddeede914d71a4ce2d9068528ed'
